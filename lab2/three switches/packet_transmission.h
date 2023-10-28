40eab985d5f557eb80a47c30e05699393a7568ee
rruiqiu <qiur12@mcmaster.ca>
2023-10-28 12:10:11 -0400

    add code

---
+++ lab2/three switches/packet_transmission.h
@@ -0,0 +1,72 @@
+
+/*
+ * 
+ * Simulation_Run of A Single Server Queueing System
+ * 
+ * Copyright (C) 2014 Terence D. Todd Hamilton, Ontario, CANADA,
+ * todd@mcmaster.ca
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+/******************************************************************************/
+
+#ifndef _PACKET_TRANSMISSION_H_
+#define _PACKET_TRANSMISSION_H_
+
+/******************************************************************************/
+
+#include "main.h"
+
+/******************************************************************************/
+
+/*
+ * Function prototypes
+ */
+
+void
+start_transmission_on_link(Simulation_Run_Ptr, Packet_Ptr, Server_Ptr);
+
+void
+end_packet_transmission_event(Simulation_Run_Ptr, void*);
+
+double
+get_packet_transmission_time(long int);
+
+long schedule_end_packet_transmission_event(Simulation_Run_Ptr, double, Server_Ptr);
+
+long schedule_end_packet_transmission_event2(Simulation_Run_Ptr, double, Server_Ptr);
+
+long schedule_end_packet_transmission_event3(Simulation_Run_Ptr, double, Server_Ptr);
+
+void
+start_transmission_on_link2(Simulation_Run_Ptr, Packet_Ptr, Server_Ptr);
+
+void
+end_packet_transmission_event2(Simulation_Run_Ptr, void*);
+
+void
+start_transmission_on_link3(Simulation_Run_Ptr, Packet_Ptr, Server_Ptr);
+
+void
+end_packet_transmission_event3(Simulation_Run_Ptr, void*);
+
+
+/******************************************************************************/
+
+#endif /* packet_transmission.h */
+
+
+
