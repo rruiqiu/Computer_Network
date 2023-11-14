a12e54b5c84a05c64023d752cbce15237ac28c02
rruiqiu <qiur12@mcmaster.ca>
2023-11-14 17:19:42 -0500

    finish lab3

---
+++ lab3/coe4dk4_lab_3_2023_q5/simparameters.h
@@ -0,0 +1,49 @@
+
+/*
+ * 
+ * Call Blocking in Circuit Switched Networks
+ * 
+ * Copyright (C) 2014 Terence D. Todd
+ * Hamilton, Ontario, CANADA
+ * todd@mcmaster.ca
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+/*******************************************************************************/
+
+#ifndef _SIMPARAMETERS_H_
+#define _SIMPARAMETERS_H_
+
+/*******************************************************************************/
+
+#define Call_ARRIVALRATE 3   /* calls/minute */
+#define MEAN_CALL_DURATION 6 /*D HERE minutes */
+#define RUNLENGTH 5e6 /* number of successful calls */
+#define BLIPRATE 1e3
+#define NUMBER_OF_CHANNELS 11
+#define G 4
+#define W 3 /*minutes*/
+/* Comma separated list of random seeds to run. */
+#define RANDOM_SEED_LIST 400318681
+
+/*******************************************************************************/
+
+#endif /* simparameters.h */
+
+
+
+
